a8eaa51ffcc1e30dccce3127102ecebaad012f81
MaltenGD <oliver.garcia.aguado@gmail.com>
2024-11-08 16:32:02 +0100

    Pequeño avance
    El pompabehaviour queda inutilizado

--- Assets/Scripts/Blowup.cs
+++ Assets/Scripts/Blowup.cs
@@ -6,15 +6,20 @@ using UnityEngine;
 
 public class Blowup : MonoBehaviour
 {
-    public float impulso_pompa = 2f;
-    public float impulso_division = 30f;
+    public float impulso_inicial = 2f;
+    public float impulso_division = 3f;
+    public Vector3 initial_size = new Vector3(1.5f, 1.5f, 1.5f);
     public Vector3 destroy_size = new Vector3(0.5f,0.5f,0.5f);
     public GameObject Pompa;
     public float division_ratio = 2f;
+    
     void Start()
     {
        GameManager.Instance.OnBubbleCreated();
-       gameObject.GetComponent<Rigidbody2D>().velocity += new Vector2(impulso_pompa, impulso_pompa);
+       if (gameObject.transform.localScale == initial_size) // aplica un impulso a las pompas iniciales
+        {
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(impulso_inicial, impulso_inicial);
+        }
 
     }
 
@@ -35,6 +40,12 @@ public class Blowup : MonoBehaviour
         Destroy(gameObject);
 
     }
+
+     void Init(int tamaño, int fuerza)
+     { 
+        //No sé que hay que hacer con este metodo
+    
+     }
      void PompaSplit()
     {
         GameObject Pompa1 = Instantiate(Pompa, transform.position + new Vector3(-0.5f, 0, 0), Quaternion.identity);
@@ -43,7 +54,7 @@ public class Blowup : MonoBehaviour
         Pompa2.transform.localScale /= division_ratio;
 
 
-        Pompa1.GetComponent<Rigidbody2D>().velocity = new Vector2(-impulso_division, impulso_division);
+        Pompa1.GetComponent<Rigidbody2D>().velocity = new Vector2(-impulso_division, impulso_division); // Aplica un impulso a las pompas divididas
         Pompa2.GetComponent<Rigidbody2D>().velocity = new Vector2(impulso_division, impulso_division);
         Debug.Log("impulso de división efectuado");
 
